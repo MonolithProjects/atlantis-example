0fc1f7fe04436880efc88409c4fa8620c71a1d4d
Michal Muransky <mike.muransky@gmail.com>
2022-02-10 14:25:28 +0100

    Update main.tf

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -4,3 +4,7 @@ resource "null_resource" "env_res" {
 module "demo_module" {
   source      = "../../modules/demo_module"
 }
+
+module "demo_module" {
+  source      = "../../modules/demo_module2"
+}
